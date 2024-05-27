86becb07ff7b27b06ed4d12acf402f033ceb8701
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2024-05-27 21:59:51 +0530

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,15 @@
+print("Hello")
+print("My name is divej.","My age is 20")
+print(20+45)
+name="divej"
+age="20"
+print(name)
+print(age)
+print(type(name))
+a=2
+b=4
+sum=a+b
+print(sum)
+print(a**b)
+name=input("enter your name:")
+
---
+++ Ques1.py
@@ -0,0 +1,3 @@
+num1=int(input("enter number 1"))
+num2=int(input("emter number 2"))
+print("sum=",num1+num2)
